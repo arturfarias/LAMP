9a57099ea445eb3a5f6333e4280a26179b8421a5
Artur <arcashaid@gmail.com>
2016-11-21 21:05:06 -0300

    tela de login

--- safira/core/views.py
+++ safira/core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from django.http import HttpResponse,HttpResponseRedirect
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
+def index(request):
+    if  request.user.is_authenticated(): # Redireciona a central caso ja esteja lo
+        return redirect(central)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid(): # verifica se e valido
+            login(request, form.get_user())
+            return HttpResponseRedirect("/central/")
+        else:
+            return render(request, "index.html", {"form": form})
+    return render(request, "index.html", {"form": AuthenticationForm()})
+
+def central(request):
+    return HttpResponse("Bem vindo a essa linda pagina em branco.")
 # Create your views here.
