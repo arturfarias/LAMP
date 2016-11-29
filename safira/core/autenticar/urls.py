36e9a866c04a1ad9d905c4ad6a72b61dafec75cf
Artur <arcashaid@gmail.com>
2016-11-29 08:45:36 -0300

    :(

--- safira/core/autenticar/urls.py
+++ safira/core/autenticar/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from core.autenticar.views import *
+from django.contrib.auth.views import password_change_done,logout
 
 urlpatterns = [
     url(r'^mudar_senha$', mudar_senha, name='mudar_senha'),
-    url(r'^mudar_senha/concluido/$', 'django.contrib.auth.views.password_change_done',{'template_name':'mudar_senha.html'},name= 'senha_alterada'),
+    url(r'^mudar_senha/concluido/$', password_change_done,{'template_name':'mudar_senha.html'},name= 'senha_alterada'),
     url(r'^registrar$', registrar,name='registrar'),
     url(r'^login$', logar,name='login'),
-    url(r'^logout$', 'django.contrib.auth.views.logout',{'next_page':'/'},name='logout'),
+    url(r'^logout$', logout,{'next_page':'/'},name='logout'),
     url(r'^ativar/(?P<codigo>\w+)/', ativar_conta,name="ativar_conta"),
 ]
