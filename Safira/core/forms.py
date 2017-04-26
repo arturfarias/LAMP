29af2b630f4e98aaa8e18010f6cf516b23fd95dd
Artur <arcashaid@gmail.com>
2017-04-26 11:09:14 -0300

    detalhes insignificantes

--- Safira/core/forms.py
+++ Safira/core/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(label='Nome')
     email = forms.EmailField(label='E-mail')
     class Meta:
         model = User
